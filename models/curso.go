734730e2f9a6b37a16772d52a579e57b6ce5e164
Ruan <camposcosta.rc@gmail.com>
2025-03-25 15:22:34 -0300

    Correção de apenas Data 7

--- models/curso.go
+++ models/curso.go
@@ -1,17 +1,33 @@
 package models
 
-import "time"
+import (
+	"strings"
+	"time"
+)
 
-// Curso representa a entidade Curso.
+type CustomTime struct {
+	time.Time
+}
+
+func (ct *CustomTime) UnmarshalJSON(b []byte) error {
+	// Remove quotes
+	s := strings.Trim(string(b), "\"")
+	// Parse using the custom layout
+	t, err := time.Parse("2006/01/02", s)
+	if err != nil {
+		return err
+	}
+	ct.Time = t
+	return nil
+}
+
 type Curso struct {
-	ID           uint      `gorm:"primaryKey;autoIncrement" json:"id"`
-	Nome         string    `gorm:"not null" json:"nome"`
-	Professor    string    `gorm:"not null" json:"professor"`
-	Data         time.Time `gorm:"not null" json:"data"`
-	CargaHoraria int32     `gorm:"not null" json:"cargaHoraria"`
-	Certificado  string    `gorm:"not null" json:"certificado"`
-
-	// Permite que a coluna aluno_id seja nula.
-	AlunoID *uint  `gorm:"default:null" json:"alunoId,omitempty"`
-	Aluno   *Aluno `gorm:"foreignKey:AlunoID" json:"aluno,omitempty"`
+	ID           uint       `gorm:"primaryKey;autoIncrement" json:"id"`
+	Nome         string     `gorm:"not null" json:"nome"`
+	Professor    string     `gorm:"not null" json:"professor"`
+	Data         CustomTime `gorm:"not null" json:"data"`
+	CargaHoraria int32      `gorm:"not null" json:"cargaHoraria"`
+	Certificado  string     `gorm:"not null" json:"certificado"`
+	AlunoID      *uint      `gorm:"default:null" json:"alunoId,omitempty"`
+	Aluno        *Aluno     `gorm:"foreignKey:AlunoID" json:"aluno,omitempty"`
 }
