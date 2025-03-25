a7747704499eaacad141e13b966d86cca0507f1a
Ruan <camposcosta.rc@gmail.com>
2025-03-25 14:30:58 -0300

    Tratamento de data

--- models/dateLayout.go
+++ models/dateLayout.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"database/sql/driver"
 	"encoding/json"
 	"fmt"
 	"time"
@@ -13,15 +14,15 @@ type CustomDate struct {
 	time.Time
 }
 
-// UnmarshalJSON implementa a desserialização do JSON para o CustomDate.
+// UnmarshalJSON desserializa a data no formato dd/MM/yyyy
 func (cd *CustomDate) UnmarshalJSON(b []byte) error {
-	// Remove as aspas
+	// Remove as aspas da string JSON
 	s := string(b)
 	if len(s) < 2 {
 		return fmt.Errorf("data inválida")
 	}
 	s = s[1 : len(s)-1]
-	// Tenta converter a string para time.Time com o layout definido
+	// Converte a string para time.Time usando o layout definido
 	t, err := time.Parse(DateLayout, s)
 	if err != nil {
 		return err
@@ -30,8 +31,43 @@ func (cd *CustomDate) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
-// MarshalJSON implementa a serialização do CustomDate para JSON.
-func (cd CustomDate) MarshalJSON() ([]byte, error) {
+// MarshalJSON serializa a data no formato dd/MM/yyyy
+func (cd *CustomDate) MarshalJSON() ([]byte, error) {
 	formatted := cd.Format(DateLayout)
 	return json.Marshal(formatted)
+}
+
+// Value implementa o método driver.Valuer para que o CustomDate seja armazenado no banco como time.Time.
+func (cd *CustomDate) Value() (driver.Value, error) {
+	return cd.Time, nil
+}
+
+// Scan implementa o método sql.Scanner para converter dados do banco para CustomDate.
+func (cd *CustomDate) Scan(value interface{}) error {
+	if value == nil {
+		cd.Time = time.Time{}
+		return nil
+	}
+
+	switch v := value.(type) {
+	case time.Time:
+		cd.Time = v
+		return nil
+	case []byte:
+		t, err := time.Parse(DateLayout, string(v))
+		if err != nil {
+			return err
+		}
+		cd.Time = t
+		return nil
+	case string:
+		t, err := time.Parse(DateLayout, v)
+		if err != nil {
+			return err
+		}
+		cd.Time = t
+		return nil
+	default:
+		return fmt.Errorf("não é possível converter o tipo %T para CustomDate", value)
+	}
 }
